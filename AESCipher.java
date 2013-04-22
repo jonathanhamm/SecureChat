7a60473a0ce3135e907965a9a864034238b96ace
Jon <jonathan-hamm@utulsa.edu>
2013-04-22 16:12:56 -0500

    Added tabs to code.

--- AESCipher.java
+++ AESCipher.java
@@ -30,26 +30,26 @@ public class AESCipher extends CipherSpi {
 
     protected void engineSetMode(String mode)
       throws NoSuchAlgorithmException {
-	if (mode.equals("CBC")) {
-	    do_cbc = true;
-	} else if (mode.equals("ECB")) {
-	    do_cbc = false;
-	} else {
-	    throw new NoSuchAlgorithmException();
-	}
+    	if (mode.equals("CBC")) {
+    		do_cbc = true;
+    	} else if (mode.equals("ECB")) {
+    		do_cbc = false;
+    	} else {
+    		throw new NoSuchAlgorithmException();
+    	}
     }
     protected void engineSetPadding(String padding)
       throws NoSuchPaddingException {
-	if (padding.equals("NoPadding")) {
-	    do_pad = false;
-	} else if (padding.equals("PKCS5Padding")) {
-	    do_pad = true;
-	} else {
-	    throw new NoSuchPaddingException();
-	}
+    	if (padding.equals("NoPadding")) {
+	    	do_pad = false;
+		} else if (padding.equals("PKCS5Padding")) {
+	    	do_pad = true;
+		} else {
+	    	throw new NoSuchPaddingException();
+		}
     }
     protected int engineGetBlockSize() {
-	return 16; // This is constant for AES.
+    	return 16; // This is constant for AES.
     }
     protected int engineGetOutputSize(int inputLen) {
 	/**
@@ -58,39 +58,39 @@ public class AESCipher extends CipherSpi {
     	return 0;
     }
     protected byte[] engineGetIV() {
-	byte[] retiv = new byte[16];
-	System.arraycopy(iv, 0, retiv, 0, 16);
-	return retiv;
+    	byte[] retiv = new byte[16];
+		System.arraycopy(iv, 0, retiv, 0, 16);
+		return retiv;
     }
     protected AlgorithmParameters engineGetParameters() {
 	AlgorithmParameters ap = null;
-	try {
-	    ap = AlgorithmParameters.getInstance("AES");
-	    ap.init(new IvParameterSpec(engineGetIV()));
-	} catch (NoSuchAlgorithmException e) {
-	    System.err.println("Internal Error: " + e);
-	} catch (InvalidParameterSpecException e) {
-	    System.err.println("Internal Error: " + e);
-	}
-	return ap;
+		try {
+	    	ap = AlgorithmParameters.getInstance("AES");
+	    	ap.init(new IvParameterSpec(engineGetIV()));
+		} catch (NoSuchAlgorithmException e) {
+	    	System.err.println("Internal Error: " + e);
+		} catch (InvalidParameterSpecException e) {
+			System.err.println("Internal Error: " + e);
+		}
+		return ap;
     }
     protected void engineInit(int opmode, Key key, SecureRandom random)
-      throws InvalidKeyException {
-	try {
-	    engineInit(opmode, key, (AlgorithmParameterSpec)null, random);
-	} catch (InvalidAlgorithmParameterException e) {
-	    System.err.println("Internal Error: " + e);
-	}
+     throws InvalidKeyException {
+    	try {
+    		engineInit(opmode, key, (AlgorithmParameterSpec)null, random);
+    	} catch (InvalidAlgorithmParameterException e) {
+    		System.err.println("Internal Error: " + e);
+    	}
     }
     protected void engineInit(int opmode, Key key, AlgorithmParameters params, SecureRandom random)
       throws InvalidKeyException {
-	try {
-	    engineInit(opmode, key, params.getParameterSpec(IvParameterSpec.class), random);
-	} catch (InvalidParameterSpecException e) {
-	    System.err.println("Internal Error: " + e);
-	} catch (InvalidAlgorithmParameterException e) {
-	    System.err.println("Internal Error: " + e);
-	}
+    	try {
+    		engineInit(opmode, key, params.getParameterSpec(IvParameterSpec.class), random);
+    	} catch (InvalidParameterSpecException e) {
+    		System.err.println("Internal Error: " + e);
+    	} catch (InvalidAlgorithmParameterException e) {
+    		System.err.println("Internal Error: " + e);
+    	}
     }
     protected void engineInit(int opmode, Key key, AlgorithmParameterSpec params, SecureRandom random)
       throws InvalidKeyException, InvalidAlgorithmParameterException {
@@ -105,14 +105,14 @@ public class AESCipher extends CipherSpi {
     	return 0;
     }
     protected byte[] engineUpdate(byte[] input, int inputOffset, int inputLen) {
-	byte[] output = new byte[allocateSize(inputLen)];
-	int size = 0;
-	try {
-	    size = engineUpdate(input, inputOffset, inputLen, output, 0);
-	} catch (ShortBufferException e) {
-	    System.err.println("Internal Error: " + e);
-	}
-	return Arrays.copyOf(output, size);
+    	byte[] output = new byte[allocateSize(inputLen)];
+    	int size = 0;
+    	try {
+    		size = engineUpdate(input, inputOffset, inputLen, output, 0);
+    	} catch (ShortBufferException e) {
+    		System.err.println("Internal Error: " + e);
+    	}
+    	return Arrays.copyOf(output, size);
     }
     protected int engineUpdate(byte[] input, int inputOffset, int inputLen, byte[] output, int outputOffset)
       throws ShortBufferException {
@@ -123,14 +123,14 @@ public class AESCipher extends CipherSpi {
     }
     protected byte[] engineDoFinal(byte[] input, int inputOffset, int inputLen)
       throws IllegalBlockSizeException, BadPaddingException {
-	try {
-	    byte[] temp = new byte[engineGetOutputSize(inputLen)];
-	    int len = engineDoFinal(input, inputOffset, inputLen, temp, 0);
-	    return Arrays.copyOf(temp, len);
-	} catch (ShortBufferException e) {
-	    System.err.println("Internal Error: " + e);
-	    return null;
-	}
+    	try {
+    		byte[] temp = new byte[engineGetOutputSize(inputLen)];
+    		int len = engineDoFinal(input, inputOffset, inputLen, temp, 0);
+    		return Arrays.copyOf(temp, len);
+    	} catch (ShortBufferException e) {
+    		System.err.println("Internal Error: " + e);
+    		return null;
+    	}
     }
     protected int engineDoFinal(byte[] input, int inputOffset, int inputLen, byte[] output, int outputOffset)
       throws ShortBufferException, IllegalBlockSizeException, BadPaddingException {
