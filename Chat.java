de1c59f6768c363e69f5ee55da989e76386038de
Jon <jonathan-hamm@utulsa.edu>
2013-04-23 18:31:57 -0500

    I think I have the Chat.java main function setting up the provider class correctly

--- Chat.java
+++ Chat.java
@@ -1,12 +1,14 @@
 import java.security.Security;
+import javax.crypto.CipherSpi;
+import csec2012.CSec2012Prov;
 
 
 
 public class Chat {
-
 	public static void main (String[] args)
 	{
-		Security.insertProviderAt(null, 1);
+		CSec2012Prov provider = new CSec2012Prov();
+		Security.insertProviderAt(provider, 1);
 	}
 }
  
